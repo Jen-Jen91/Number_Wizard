c5133343a764de1bdcb15b24d22ec6e3e198cd03
Jen Proctor <jen_p_13@hotmail.co.uk>
2019-04-28 16:25:15 +0100

    Added logs to console and max/min variables

--- Number Wizard Console/Assets/NumberWizard.cs
+++ Number Wizard Console/Assets/NumberWizard.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class NumberWizard : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
+
+        int maxGuess = 1000;
+        int minGuess = 1;
+
         Debug.Log("Welcome to Number Wizard!");
+        Debug.Log("Pick a number...");
+        Debug.Log("The highest number you can choose is: " + maxGuess);
+        Debug.Log("The lowest number you can choose is: " + minGuess);
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 }
